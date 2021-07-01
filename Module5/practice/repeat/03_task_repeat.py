aa01e9b3c7df4a2a78d9d6bf14e6f33c7472d8df
Konstantin0804 <86655035+Konstantin0804@users.noreply.github.com>
2021-07-01 18:16:13 +0300

    Update 03_task_repeat.py

--- Module5/practice/repeat/03_task_repeat.py
+++ Module5/practice/repeat/03_task_repeat.py
@@ -3,3 +3,17 @@
 
 a = ...
 b = ...
+
+def palindrome(number):
+    n_str = str(number)
+    n_rev = n_str[::-1]
+    return n_str == n_rev
+
+a = 145
+b = 1690
+count = 0
+my_list=list(range(a,b))
+for i in my_list:
+    if palindrome(i) :
+        count += 1
+print(f"{count} палиндромов между {a} и {b}")
